7542b8bcc1a5eb1a6a6851864b28d659cf52eb7d
Michał Rajzer <mmrajzer@gmail.com>
2019-04-09 15:01:36 +0200

    Finished 'self' exercise

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 function Phone(brand, price, color, system) {
 	this.brand = brand;
 	this.price = price;
@@ -20,3 +22,22 @@ One.printInfo();
 Iphone6S.sysType();
 GalaxyS6.sysType();
 One.sysType();
+
+
+function Button(text){
+	this.text = text || 'Hello';
+}
+Button.prototype = {
+	create: function(){
+		var self = this;
+		this.element = document.createElement('button');
+		this.element.innerText = this.text;
+		this.element.addEventListener('click', function(){
+			alert(self.text);
+		});
+		document.body.appendChild(this.element);
+	}
+}
+
+var btn1 = new Button('Hello');
+btn1.create();
